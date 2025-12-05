c858d5fff5a4326421b38cb48011db3272ad0252
Maria Eduarda Vassão <mariaeduarda9soares@gmail.com>
2025-12-05 17:56:04 -0300

    Update ft_momvome.c

--- ft_momvome.c
+++ ft_momvome.c
@@ -1,21 +1,27 @@
 #include "libft.h"
 
 void *ft_momvome(void*dst, const void *src, size_t len){
-    unsigned char *temp_dst;
-    unsigned char *temp_src;
-    size_t cont;
-    if (dst == (void*)0 && src == (void*)0)
+    char *temp_dst;
+    char *temp_src;
+    size_t i = 0;
+    if (!dst && !src)
     {
-        return dst;
+        return NULL;
     }
-    temp_dst = (unsigned char *)dst;
-    temp_src = (unsigned char *)src;
+    temp_dst = ( char *)dst;
+    temp_src = ( char *)src;
 
-    if ()
+    if (temp_dst > temp_src)
     {
-        /* code */
+        while (len > 0)
+        {
+            temp_dst[len] = temp_src[len];
+        }
+    } else{
+        while (i++ < len)
+        {
+            temp_dst[i] = temp_src[i];
+        }
     }
-    
-    
-
+    return dst;
 }
