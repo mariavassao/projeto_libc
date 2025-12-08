010035cfefe9e6611c74182766f5a247b3b2f939
Maria Eduarda Vassão <mariaeduarda9soares@gmail.com>
2025-12-08 16:40:45 -0300

    sad

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,23 +1,26 @@
 #include "libft.h"
 
-size_t ft_strlcat( char * dst, const char *src, size_t dstsize){
-    size_t src_len, dst_len;
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+    size_t    src_len;
+    size_t    dst_len;
+    
+  
     src_len = ft_strlen(src);
     dst_len = ft_strlen(dst);
+  
+    if (dst_len >= dstsize)
+        dst_len = dstsize;
+   
+    if (dst_len == dstsize)
+        return (dstsize + src_len);
 
-    if (dst_len <= dstsize + 1)
-    {
-        return dst_len + src_len;
-    } 
-    else if (dstsize + 1 > (dst_len + src_len))
-    {
-        dst = *dst + *src;
-    } 
+    if (src_len < dstsize - dst_len)
+        ft_memcpy(dst + dst_len, src, src_len + 1);
     else
     {
-        
+        ft_memcpy(dst + dst_len, src, dstsize - dst_len - 1);
+        dst[dstsize - 1] = '\0';
     }
-    
-    
-
+    return (dst_len + src_len);
 }
