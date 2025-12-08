d1c06b72c0732505d7f0ff9db214053f8fdd22cc
Maria Eduarda Vassão <mariaeduarda9soares@gmail.com>
2025-12-08 20:41:52 -0300

    implementação teste

---
+++ ft_atoi.c
@@ -0,0 +1,39 @@
+#include "libft.h"
+
+int    ft_atoi(const char *str)
+{
+    int    result;
+    int    sign;
+    int    i;
+    
+    result = 0;
+    sign = 1;
+    i = 0;
+
+    while (ft_isspace(str[i]))
+        i++;
+
+    if (str[i] == '+' && str[i + 1] != '-')
+        i++;
+
+    if (str[i] == '-')
+    {
+        sign = -1;
+        i++;
+    }
+    while (str[i] && str[i] >= 48 && str[i] <= 57)
+    {
+        result *= 10;
+	result += str[i] - 48;
+	i++;
+    }
+    result *= sign;
+    return (result);
+}
+
+int    ft_isspace(int c)
+{
+    if (c == 9 || c == 10 || c == 11 || c == 12 || c == 13 || c == 32)
+        return (1);
+    return (0);
+}
