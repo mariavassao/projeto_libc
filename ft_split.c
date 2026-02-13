86712faac0f7a75141384203c2574027e67f6db0
Maria Eduarda Vassão <mariaeduarda9soares@gmail.com>
2026-02-13 11:29:16 -0300

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -1,15 +1,9 @@
 #include "libft.h"
 
 char **ft_split(const char *s, char c){
-    /*
-        chama função word_count passando a string para verificação de quantidade
-        aloca memoria
-        percorre a string s
-        encontra o inicio da palavra e chama fill para copiar e retonar UMA palavra
-    */
    int n = word_count(s, c);
    int i = 0;
-   char *nova_array = (char*) malloc(n+1);
+   char **nova_array = malloc(sizeof(char*) * (n+1));
    while ( s[i] == c)
    {
      fill_word(s[i], c);
@@ -17,39 +11,32 @@ char **ft_split(const char *s, char c){
    
 }
 
-int word_count(const char *i, char j){
-    /*
-        percorre a string e pula o delimitador 
-        conta quantas palavras resultantes tem e retorna o valor
-    */
-   int p = 0;
-   int quantidade;
-   while (i[p] != j)
-   {
-     quantidade++;
-     p++;
-   }
-   return quantidade;
-   
+int word_count(const char *s, char c){
+   int i = 0;
+    int cont = 0;
+    while (s[i])
+    {
+       if (s[i] == c)
+       {
+        i++;
+       } else
+       {
+        cont++;
+       }
+       
+    }
+    
 }
 
 void ft_free( ){
-    /*
-        libera a memoria se algo der erro
-    */
+
 }
 
 char *fill_word(char *nova, char d){
-    /*
-       recebe o inicio da palavra 
-       copia uma palavra 
-       aloca memoria
-       e retorna a palavra para o split adicionar no char 
-    */
     int cont = 0; 
     char *novastring;
     for (size_t i = 0; i < nova; i++)
     {
-        /* code */
+       
     }
 }
