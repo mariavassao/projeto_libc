e2a7442cf42b45a0b33eacc003fc2b26c24b09ea
Maria Eduarda Vassão <mariaeduarda9soares@gmail.com>
2026-02-18 18:31:58 -0300

    função finalizada

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,5 +1,18 @@
 #include "libft.h"
 
 char *ft_strmapi(const char *s, char (*f)(unsigned int, char)){
-    
+    unsigned int i = 0;
+    char *str = malloc(sizeof(char) * ft_strlen(s) + 1); 
+    if (!str)
+    {
+        return(NULL);
+    }
+
+    while (s[i])
+    {
+        str[i] = f(i, s[i]);
+        i++;
+    }
+    str[i] = '\0';
+    return str;
 }
