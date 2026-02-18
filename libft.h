c47284b6fbe68c88e32695f3fbde339531be516e
Maria Eduarda Vassão <mariaeduarda9soares@gmail.com>
2026-02-18 12:44:00 -0300

    ft strmapi

--- libft.h
+++ libft.h
@@ -32,4 +32,5 @@ char *ft_strjoin(const char *s1, const char *s2);
 char *ft_sttrirm(const char *s1, const char *set);
 char **ft_split(const char *s, char c);
 char *ft_itoa( int n);
+char *ft_strmapi(const char *s, char (*f)(unsigned int, char));
 #endif
--- main.c
+++ main.c
@@ -1,10 +1,24 @@
 #include "libft.h"
 
-int main(void)
+char	alternar_case(unsigned int i, char c)
 {
-    char *str;
+	if (c >= 'a' && c <= 'z' && i % 2 == 0)
+		return (c - 32);
+	if (c >= 'A' && c <= 'Z' && i % 2 != 0)
+		return (c + 32);
+	return (c);
+}
 
-    str = ft_itoa(-2024);
-    printf("%s\n", str);
-    return (0);
+int	main(void)
+{
+	char *original = "casa42sp";
+	char *resultado;
+
+	resultado = ft_strmapi(original, alternar_case);
+
+	printf("Original: %s\n", original);
+	printf("Resultado: %s\n", resultado);
+
+	free(resultado);
+	return (0);
 }
