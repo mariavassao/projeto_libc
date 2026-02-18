ca95b2a694ea345bab02fa56ce172bcc0560cf49
Maria Eduarda Vassão <mariaeduarda9soares@gmail.com>
2026-02-18 12:27:18 -0300

    commit

--- ft_itoa.c
+++ ft_itoa.c
@@ -3,7 +3,9 @@
 static int len(int n){
     int i = 0;
 
-    while (n)
+    
+
+    while (n != 0)
     {    
         n /= 10;
         i++;
@@ -11,26 +13,25 @@ static int len(int n){
     return i;
 }
 
+
 char *ft_itoa( int n){
     int tamanho = len(n);
     char *str = malloc(tamanho+1);
     
-    // if (n < 0)
-    // {
-    //     str[0] = '-';
-    //     tamanho++;
-    //     n = -n;
-    // }
-
+    if (n < 0)
+    {
+       str[0] = '-';
+       n = -n;
+    }
+   
     int i = tamanho - 1;
-
     while (i >= 0)
     {
         str[i] = (n % 10) + '0';
         n/=10;
         i--;
     }
-   
+
     str[tamanho] = '\0';
     
     return str;
